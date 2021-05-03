c91ffe4311e62a44b68785498b13e0f59b0ede2c
Hye-Rin-Lee <49551361+Hye-Rin-Lee@users.noreply.github.com>
2021-05-03 23:44:49 +0900

    test : 위너의 position 찾기 테스트

--- src/test/java/racingGame/CarsTest.java
+++ src/test/java/racingGame/CarsTest.java
@@ -2,10 +2,26 @@ package racingGame;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class RacingGameTest {
+public class CarsTest {
+
+	Cars cars ;
+
+	@BeforeEach
+	void setUp(){
+		List<Car> carList = new ArrayList<>();
+		carList.add(new Car("aa",2) );
+		carList.add(new Car("bb",1));
+		carList.add(new Car("cc",3));
+		cars = new Cars(carList);
+	}
 
 	@Test
 	@DisplayName("차생성 테스트")
@@ -15,7 +31,7 @@ public class RacingGameTest {
 	}
 
 	@Test
-	@DisplayName("차 move 테스트")
+	@DisplayName("차 위치 변경 테스트")
 	public void carMove() {
 		Car car = new Car("car");
 		assertThat(car.getPosition()).isEqualTo(0);
@@ -23,5 +39,11 @@ public class RacingGameTest {
 		assertThat(car.getPosition()).isEqualTo(0);
 		car.race(new RandomNumber(4));
 		assertThat(car.getPosition()).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName(" winner Position 찾기")
+	public void getMaxPosition(){
+		assertThat(cars.getMaxPosition()).isEqualTo(3);
 	}
 }
