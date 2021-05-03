4b6501004df124bd7daa15f430ed98761ba9849b
Hye-Rin-Lee <49551361+Hye-Rin-Lee@users.noreply.github.com>
2021-05-03 23:57:11 +0900

    feat : RacingGame 메인 클래스 추가

---
+++ src/main/java/racingGame/RacingGame.java
@@ -0,0 +1,10 @@
+package racingGame;
+
+import manager.GameManager;
+
+public class RacingGame {
+	public static void main(String[] args) {
+		GameManager manager = new GameManager();
+		manager.play();
+	}
+}
