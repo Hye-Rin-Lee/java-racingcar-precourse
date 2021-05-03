97f23abea7d6685da5db42908cc7c7517d8e33f3
Hye-Rin-Lee <49551361+Hye-Rin-Lee@users.noreply.github.com>
2021-05-03 23:52:08 +0900

    feat : position 문자열로 리턴, race 메서드 position 리턴하도록 수정

--- src/main/java/racingGame/Car.java
+++ src/main/java/racingGame/Car.java
@@ -12,20 +12,33 @@ public class Car {
 			throw new IllegalArgumentException();
 		}
 		this.name = name;
-		this.position = 0;
+	}
+
+	public Car(String name, int position) {
+		this.name = name;
+		this.position = position;
 	}
 
 	public String getName() {
 		return this.name;
 	}
 
-	public int getPosition(){
+	public int getPosition() {
 		return this.position;
 	}
 
-	public void race(RandomNumber randomNumber) {
+	public String getPositionString() {
+		String positionString = "";
+		for (int i = 0; i < position; i++) {
+			positionString = positionString.concat("-");
+		}
+		return positionString;
+	}
+
+	public int race(RandomNumber randomNumber) {
 		if (randomNumber.isMovable()) {
 			position++;
 		}
+		return position;
 	}
 }
