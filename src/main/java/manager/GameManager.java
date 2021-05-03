1606a2e83ee1ef5d6e9150bcde8ac7aa37bfbfdb
Hye-Rin-Lee <49551361+Hye-Rin-Lee@users.noreply.github.com>
2021-05-03 23:56:17 +0900

    feat : 사용자 입력 받는 기능 커밋

---
+++ src/main/java/manager/GameManager.java
@@ -0,0 +1,32 @@
+package manager;
+
+import java.util.Scanner;
+
+import racingGame.Cars;
+import racingGame.RandomNumberGenerator;
+
+public class GameManager {
+
+	private Cars cars;
+	private RandomNumberGenerator generator;
+	private int attempt;
+
+	public GameManager() {
+		generator = new RandomNumberGenerator();
+		attempt = 0;
+	}
+
+	public void play() {
+		initGame();
+	}
+
+	public void initGame() {
+		System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+		Scanner scanner = new Scanner(System.in);
+		String carNames = scanner.next();
+		cars = new Cars(carNames);
+		System.out.print("시도할 회수는 몇회인가요?\n");
+		attempt = scanner.nextInt();
+	}
+
+}
