e59afb825dc71d5fbe4e73bd6e23a82c91956e28
Hye-Rin-Lee <49551361+Hye-Rin-Lee@users.noreply.github.com>
2021-05-03 21:45:55 +0900

    feat : 자동차 이름 길이 체크 유틸 기능 추가

--- src/main/java/utils/ValidationUtils.java
+++ src/main/java/utils/ValidationUtils.java
@@ -4,8 +4,13 @@ public class ValidationUtils {
 
 	private static final int MIN = 1;
 	private static final int MAX = 9;
+	private static final int MAX_NAME_LENGTH = 5;
 
 	public static boolean validRange(int no) {
 		return no >= MIN && no <= MAX;
+	}
+
+	public static boolean checkLength(String name){
+		return name.length() <= MAX_NAME_LENGTH;
 	}
 }
